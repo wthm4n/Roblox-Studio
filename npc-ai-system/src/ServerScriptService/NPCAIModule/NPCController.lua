9658e0e12b5d4d85db49095d6cab96036db5aba8
wthm4n <noenoeke11@gmail.com>
2026-02-27 15:39:26 +0530

    auto: update npc-ai-system/src/ServerScriptService/NPCAIModule/NPCController.lua — 3:39:26 pm

--- npc-ai-system/src/ServerScriptService/NPCAIModule/NPCController.lua
+++ npc-ai-system/src/ServerScriptService/NPCAIModule/NPCController.lua
@@ -322,6 +322,7 @@ function NPCController:_createConeParts()
 		w.CastShadow  = false
 		w.Material    = Enum.Material.Neon
 		w.Transparency = 0.9
+		w.Color 	   = Color3.fromRGB(180, 180, 180)
 		w.Size         = Vector3.new(0.1, 0.1, 0.1)
 		w.Parent       = sub
 		table.insert(parts, w)
